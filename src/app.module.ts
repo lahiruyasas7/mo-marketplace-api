2fadc7f953bc3f2884fe9c3c7d050c50a3cb9d8f
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-03-28 23:57:23 +0530

    chore(database): setup TypeORM with PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfigModule } from './config/app-config/app.config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/databse-config/typeorm-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/databse-config/data-source.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/config/database/migrations/*.ts'],
+
+  synchronize: false,
+});
