5e646afcdc974977b9d149642ab0d35115c02a45
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-03-30 14:19:11 +0530

    format: formatted code base

--- src/config/database/migrations/1774844722859-Init.ts
+++ src/config/database/migrations/1774844722859-Init.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1774844722859 implements MigrationInterface {
-    name = 'Init1774844722859'
+  name = 'Init1774844722859';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "refresh_tokens" RENAME COLUMN "updatedAt" TO "expireAt"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "refresh_tokens" RENAME COLUMN "updatedAt" TO "expireAt"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "refresh_tokens" RENAME COLUMN "expireAt" TO "updatedAt"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "refresh_tokens" RENAME COLUMN "expireAt" TO "updatedAt"`,
+    );
+  }
 }
