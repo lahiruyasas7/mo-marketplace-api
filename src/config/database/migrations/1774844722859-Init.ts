d4cfd17e7dbbcedb0e02ec11926ca3d91d1a42a2
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-03-30 09:57:38 +0530

    chore: migration file for update expire column

---
+++ src/config/database/migrations/1774844722859-Init.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1774844722859 implements MigrationInterface {
+    name = 'Init1774844722859'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" RENAME COLUMN "updatedAt" TO "expireAt"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" RENAME COLUMN "expireAt" TO "updatedAt"`);
+    }
+
+}
