0efcd2a3d49f11929028e2bf793941e83af6772a
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-04-03 21:58:54 +0530

    feat: created order entity

---
+++ src/entities/order.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+import { OrderItem } from './order-item.entity';
+
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User, (user) => user.orders)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column({ type: 'numeric' })
+  total_amount: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @OneToMany(() => OrderItem, (item) => item.order, {
+    cascade: true,
+  })
+  items: OrderItem[];
+}
